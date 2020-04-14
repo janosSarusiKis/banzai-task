e1445f865dea306d79ea045e3759b07aeae7ad4c
Janos Sarusi-Kis <janos.sarusikis@gmail.com>
2020-04-14 20:34:06 +0200

    Update annotations

--- controllers/customingressmanager_controller.go
+++ controllers/customingressmanager_controller.go
@@ -56,7 +56,8 @@ type CustomIngressManagerReconciler struct {
 
 // +kubebuilder:rbac:groups=webapp.feladat.banzaicloud.io,resources=customingressmanagers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=webapp.feladat.banzaicloud.io,resources=customingressmanagers/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups="",resources=services;ingresses;clusterissuers,verbs=get;list;create;update;delete
+// +kubebuilder:rbac:groups="",resources=services;ingresses;clusterissuers,verbs=get;list;create;update;delete,watch
+// +kubebuilder:rbac:groups=extensions;cert-manager.io,resources=services;ingresses;clusterissuers,verbs=get;list;create;update;watch
 
 func (r *CustomIngressManagerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
